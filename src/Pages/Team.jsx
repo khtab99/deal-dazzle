b5358752d17779c7df471f67a44b17d4c33a82a7
khtab <khtabmax78@gmail.com>
2023-06-09 00:26:05 +0400

    /

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Footer from "../components/Footer";
-import HeroPages from "../components/HeroPages";
-import Person1 from "../images/team/1.png";
-import Person2 from "../images/team/2.png";
-import Person3 from "../images/team/3.png";
-import Person4 from "../images/team/4.png";
-import Person5 from "../images/team/5.png";
-import Person6 from "../images/team/6.png";
+import React from 'react';
+import Footer from '../components/Footer';
+import HeroPages from '../components/HeroPages';
+import Person1 from '../images/team/1.png';
+import Person2 from '../images/team/2.png';
+import Person3 from '../images/team/3.png';
+import Person4 from '../images/team/4.png';
+import Person5 from '../images/team/5.png';
+import Person6 from '../images/team/6.png';
 
-function Team() {
+const Team = () => {
   const teamPpl = [
-    { img: Person1, name: "Luke Miller", job: "Salesman" },
-    { img: Person2, name: "Michael Diaz", job: "Business Owner" },
-    { img: Person3, name: "Briana Ross", job: "Photographer" },
-    { img: Person4, name: "Lauren Rivera", job: "Car Detailist" },
-    { img: Person5, name: "Martin Rizz", job: "Mechanic" },
-    { img: Person6, name: "Caitlyn Hunt", job: "Menager" },
+    { img: Person1, name: 'Luke Miller', job: 'Salesman' },
+    { img: Person2, name: 'Michael Diaz', job: 'Business Owner' },
+    { img: Person3, name: 'Briana Ross', job: 'Photographer' },
+    { img: Person4, name: 'Lauren Rivera', job: 'Car Detailist' },
+    { img: Person5, name: 'Martin Rizz', job: 'Mechanic' },
+    { img: Person6, name: 'Caitlyn Hunt', job: 'Menager' },
   ];
   return (
     <>
@@ -52,6 +52,6 @@ function Team() {
       </section>
     </>
   );
-}
+};
 
 export default Team;
